000bafa273961190fab009899fecede13e99c805
Kevin <kakurineuin@gmail.com>
2019-01-21 16:57:51 +0800

    實作 Graph 廣度優先搜尋。

--- graph/main.go
+++ graph/main.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 )
 
+// Graph 圖。
 type Graph struct {
 	AdjacencyList map[string][]string
 }
 
-/**
- * 加入節點。
- */
+// AddVertex 加入節點。
 func (g *Graph) AddVertex(vertex string) {
 	if _, ok := g.AdjacencyList[vertex]; ok {
 		return
@@ -19,25 +18,19 @@ func (g *Graph) AddVertex(vertex string) {
 	g.AdjacencyList[vertex] = make([]string, 0)
 }
 
-/**
- * 加入邊。
- */
+// AddEdge 加入邊。
 func (g *Graph) AddEdge(vertex1, vertex2 string) {
 	g.AdjacencyList[vertex1] = append(g.AdjacencyList[vertex1], vertex2)
 	g.AdjacencyList[vertex2] = append(g.AdjacencyList[vertex2], vertex1)
 }
 
-/**
- * 移除邊。
- */
+// RemoveEdge 移除邊。
 func (g *Graph) RemoveEdge(vertex1, vertex2 string) {
 	g.AdjacencyList[vertex1] = g.remove(g.AdjacencyList[vertex1], vertex2)
 	g.AdjacencyList[vertex2] = g.remove(g.AdjacencyList[vertex2], vertex1)
 }
 
-/**
- * 移除節點。
- */
+// RemoveVertex 移除節點。
 func (g *Graph) RemoveVertex(vertex string) {
 	for len(g.AdjacencyList[vertex]) > 0 {
 		g.RemoveEdge(vertex, g.AdjacencyList[vertex][0])
@@ -46,9 +39,7 @@ func (g *Graph) RemoveVertex(vertex string) {
 	delete(g.AdjacencyList, vertex)
 }
 
-/**
- * 深度優先搜尋。
- */
+// DepthFirstSearch 深度優先搜尋。
 func (g *Graph) DepthFirstSearch(start string) []string {
 	result := make([]string, 0)
 	visited := make(map[string]bool)
@@ -71,6 +62,32 @@ func (g *Graph) DepthFirstSearch(start string) []string {
 	return result
 }
 
+// BreadthFirstSearch 廣度優先搜尋。
+func (g *Graph) BreadthFirstSearch(start string) []string {
+	result := make([]string, 0)
+	visited := make(map[string]bool)
+	queue := make([]string, 0)
+	visited[start] = true
+	queue = append(queue, start)
+
+	for len(queue) > 0 {
+		vertex := queue[0]
+		queue = queue[1:]
+		result = append(result, vertex)
+
+		for _, adjacencyVertex := range g.AdjacencyList[vertex] {
+			if _, ok := visited[adjacencyVertex]; ok {
+				continue
+			}
+
+			visited[adjacencyVertex] = true
+			queue = append(queue, adjacencyVertex)
+		}
+	}
+
+	return result
+}
+
 func (g *Graph) remove(vertexs []string, vertex string) []string {
 	index := -1
 
@@ -88,13 +105,13 @@ func (g *Graph) remove(vertexs []string, vertex string) []string {
 }
 
 func main() {
-	//     A
-	//   /  \
-	//  B    C
-	//  |    |
-	//  D----E
-	//  \   /
-	//    F
+	//       A
+	//     /  \
+	//   /     \
+	//  B       C
+	//  |\    / |
+	// |  \  /  |
+	// D---E----F
 	graph := Graph{AdjacencyList: make(map[string][]string)}
 	graph.AddVertex("A")
 	graph.AddVertex("B")
@@ -106,10 +123,12 @@ func main() {
 	graph.AddEdge("A", "B")
 	graph.AddEdge("A", "C")
 	graph.AddEdge("B", "D")
+	graph.AddEdge("B", "E")
 	graph.AddEdge("C", "E")
+	graph.AddEdge("C", "F")
 	graph.AddEdge("D", "E")
-	graph.AddEdge("D", "F")
 	graph.AddEdge("E", "F")
 
-	fmt.Println(graph.DepthFirstSearch("A"))
+	fmt.Println(graph.DepthFirstSearch("A"))   // A, B, D, E, C, F
+	fmt.Println(graph.BreadthFirstSearch("A")) // A, B, C, D, E, F
 }
